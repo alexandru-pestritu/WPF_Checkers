68c723e6af6b428250ff2aa91f99dfdea2eaef9d
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-04-12 21:53:04 +0300

    Added Piece model

---
+++ Models/Piece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Checkers.Models
+{
+    public class Piece
+    {
+        public static string BlackPiece = "C:\\Users\\pestr\\Source\\Repos\\WPF_Checkers\\Resources\\Images\\BlackPiece.png";
+        public static string RedPiece = "C:\\Users\\pestr\\Source\\Repos\\WPF_Checkers\\Resources\\Images\\RedPiece.png";
+        public static string RedKingPiece = "C:\\Users\\pestr\\Source\\Repos\\WPF_Checkers\\Resources\\Images\\KingRedPiece.png";
+        public static string BlackKingPiece = "C:\\Users\\pestr\\Source\\Repos\\WPF_Checkers\\Resources\\Images\\KingBlackPiece.png";
+    }
+}
