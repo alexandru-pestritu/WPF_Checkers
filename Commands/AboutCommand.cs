0a734893322bc4f28016257aaa5073459fff7daf
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-04-15 14:29:23 +0300

    About command

---
+++ Commands/AboutCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Wpf_Checkers.Views;
+
+namespace Wpf_Checkers.Commands
+{
+    class AboutCommand : ICommand
+    {
+        public AboutCommand() { }
+
+        public void ShowAbout()
+        {
+            About aboutWindow = new About();
+            aboutWindow.Show();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ShowAbout();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
