4ac0aa02df5882dba5ca2dde59fa4f8141108642
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-04-15 14:58:11 +0300

    Bool to visibility convertor

---
+++ Utils/BoolToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Wpf_Checkers.Utils
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool && (bool)value)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Hidden; 
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
